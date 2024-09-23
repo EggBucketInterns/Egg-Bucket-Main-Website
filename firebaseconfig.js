74f910802960c15e465ca4923c03e10ba20046bb
GURSIMRANJEET SINGH <gursimranjeet10singh@gmail.com>
2024-09-23 14:18:51 +0530

    Update firebaseconfig.js

--- firebaseconfig.js
+++ firebaseconfig.js
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth';
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCA3LHBnhpgI9UstaaewiqSZeVKx4AsSPI",
-  authDomain: "b2c-main.firebaseapp.com",
-  projectId: "b2c-main",
-  storageBucket: "b2c-main.appspot.com",
-  messagingSenderId: "644465249792",
-  appId: "1:644465249792:web:4d64b23d65d401fbd292e5",
-  measurementId: "G-SBL116DGDY"
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGE_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
